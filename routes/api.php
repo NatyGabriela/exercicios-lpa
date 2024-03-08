58dc1b2373d7167a2de5f7c1e91d9c738c5b3151
NatyGabriela <nataliaggb007@gmail.com>
2024-03-08 10:28:48 -0300

    exercicio 3

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,17 @@ Route::get('nome/users', function (Request $request){
     $nome = 'naty';
    return $nome;
     });
-    
+
     Route::get('nome/idade' , function (Request $request){
         $nome = 'naty';
         $idade = '16' ;
         return "meu nome é" . $nome . " ,tenho" . $idade . "anos de idade";
         }); 
+        
+        Route::get('nome/cidade', function(Request $request){
+            $nome = "naty" ;
+            $ano = "2007" ;
+            $cidade = "Presidente Epitácio" ;
+            return "meu nome é" . $nome . "nasci no ano de " .  "na cidade de" . $cidade;     
+        });
+
