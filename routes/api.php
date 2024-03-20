d7a4874a294687d5d1ef67983fc3de4992b10a81
Natygabriela <nataliaggb07@gmail.com>
2024-03-20 15:54:25 -0300

    aula exercicios if

--- routes/api.php
+++ routes/api.php
@@ -134,3 +134,85 @@ Route::get('produto', function (Request $request) {
     $resultado = "valor total: " . $precoProduto * $quantidade;
     return $resultado;
 });
+
+Route::get('verifica/idade', function(Request $request){
+$idade = $request->input('idade');
+
+if($idade > 18){
+    return "maior de idade";
+} else {
+    return "menor de idade";
+}
+});
+
+Route::get('verificar/par', function(Request $request){
+$numero = $request->input('numero');
+
+if($numero % 2 == 0){
+    return 'par';
+} else {
+    return 'impar';
+}
+
+});
+
+Route::get('verificar/numero', function (Request $request){
+$numero = $request->input('numero');
+
+if($numero >= 10){
+    return 'maior numero';
+    
+} else{
+    return 'menor numero';
+}
+});
+
+Route::get('temperatura', function (Request $request){
+$temperatura = $request->input('numero');
+
+if($temperatura >= 30){
+    return 'esta quente';
+}else{
+    return 'nao esta quente';
+}
+});
+
+Route::get('positivo/negativo', function (Request $request){
+$numero = $request->input('numero');
+if ($numero >0){
+    return 'este numero e positivo';
+
+}else if($numero <0){
+    return 'este numero e negativo';
+
+}else {
+    return 'este numero e zero';
+}
+});
+
+Route::get('maior/numero', function (Request $request){
+$numero1 = $request->input('numero1');
+$numero2 = $request->input('numero 2');
+if ('$numero1 > $numero2'){
+    return 'o numero maior e ' . $numero1;
+
+} else {
+    return 'o numero menor e ' . $numero2;
+}
+
+
+});
+
+Route::get('divisao/tres', function (Request $request){
+    $numero =$request->input('numero');
+    $resto = ($numero %3);
+    if($resto ==0){
+    return 'numero e divisivel';
+    }else{
+        return 'numero nao e divisivel';
+    }
+});
+
+
+
+
